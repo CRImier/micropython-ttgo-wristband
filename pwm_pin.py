efad0c17bb033e357d5a713e0073d96aa349615f
CRImier <crimier@yandex.ru>
2020-04-21 01:14:29 +0300

    Adding getting the pin's state to PWMPin

--- pwm_pin.py
+++ pwm_pin.py
@@ -11,18 +11,18 @@ class PWMPin():
         self.pwm = PWM(self.p, duty=init_duty)
         self.on_duty = on_duty
         self.off_duty = off_duty
-        self.on = init_value
+        self.is_on = bool(init_value)
 
     def on(self):
-        self.on = True
+        self.is_on = True
         self.pwm.duty(self.on_duty)
 
     def off(self):
-        self.on = False
+        self.is_on = False
         self.pwm.duty(self.off_duty)
 
     def value(self, value=None):
-        if value is None: return self.on
+        if value is None: return self.is_on
         self.on() if value else self.off()
 
     def duty(self, duty=None):
